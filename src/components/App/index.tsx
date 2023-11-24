a0f2126c6dffb3718bda4fde41f820081c959ce3
Colette <hello@colettesmith.com>
2023-11-24 14:50:00 +0000

    authentication cont

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,18 +1,39 @@
+import { getAccessToken } from "../../auth";
 import Login from "../Login";
 import TrackInfo from "../TrackInfo";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
   const [token, setToken] = useState<string | null>(null);
 
+  useEffect(() => {
+    getToken();
+  }, []);
+
+  const getToken = async () => {
+    //instantiating some variables
+    const clientId = import.meta.env.VITE_CLIENT_ID;
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code");
+
+    if (code) {
+      const accessToken = await getAccessToken(clientId, code);
+      setToken(accessToken);
+    }
+  };
+
   if (!token) {
     return (
       <>
-        <Login></Login>
+        <Login />
       </>
     );
   } else {
-    <TrackInfo></TrackInfo>;
+    return (
+      <>
+        <TrackInfo />
+      </>
+    );
   }
 };
 
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,8 +1,15 @@
+import { redirectToAuthCodeFlow } from "../../auth";
+
 const Nav = () => {
+  const clientId = import.meta.env.CLIENT_ID;
+
+  const handleClick = async () => {
+    await redirectToAuthCodeFlow(clientId);
+  };
   return (
     <>
       <h1> Brutalist Music</h1>
-      <button>Login</button>
+      <button onClick={handleClick}>Login</button>
     </>
   );
 };
